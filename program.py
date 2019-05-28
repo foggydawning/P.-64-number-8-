b97b2aef51f93e2f8b4ea74de89e53e3bdce8e47
ILYA CHUB <iliachubich@gmail.com>
2019-05-28 23:10:36 +0300

    Добавлена функция сортировки "пузырьком"

--- program.py
+++ program.py
@@ -14,3 +14,23 @@ def sort_kamnem (list):
         list.insert(0,li[k])
 
     return(list)
+
+
+def sort_puzir (list):
+    if len(list)==1:
+        return list
+
+    i=len(list)-1
+
+    while i>0:
+        if list[i]<list[i-1]:
+            list.insert(i-1, list.pop(i))
+        i-=1
+
+    li = sort (list[1:])
+    del list[-1:0:-1]
+
+    for k in range (0, len(li)):
+        list.append(li[k])
+
+    return list
