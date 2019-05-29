5d897b12645e09c95df6922cbaa14bac922beeea
ILYA CHUB <iliachubich@gmail.com>
2019-05-30 02:45:28 +0300

    1

--- program.py
+++ program.py
@@ -1,14 +1,17 @@
-def sort_kamnem (list, last):
+def sort_kamnem (list, last, per=1):
     if -1>=last:
         list.reverse()
+        print("Количество перестановок при методе 'камня' =", per)
         return
     i=list.index(max(list[0:last+1]))
     list.append(list[i])
     list.pop(i)
+    per+=1
     sort_kamnem (list, last-1)
 
-def sort_puzir (list, first=0):
+def sort_puzir (list, first=0,per=0):
     if first==len(list):
+        print("Количество перестановок при использовании метода 'пузырька' =", per)
         return
 
     i=len(list)-1
@@ -16,13 +19,15 @@ def sort_puzir (list, first=0):
     while i>0:
         if list[i]<list[i-1]:
             list[i] , list [i-1] = list [i-1], list[i]
+            per+=1
         i-=1
 
     sort_puzir (list, first+1)
 
-def quicksort (list, min, max):
+def quicksort (list, min, max, per=0):
     if min>=max:
-        return
+        print("Количество перестановок при использовании метода 'быстрой сортировки Хоара' =", per)
+        return per
 
     i=min
     j=max
@@ -36,7 +41,8 @@ def quicksort (list, min, max):
 
         if i <= j:
             list[i], list[j]=list[j], list[i]
+            per+=1
             i+=1
             j-=1
-            sort ( list, min, j )
-            sort ( list, i, max )
+            quicksort ( list, min, j, per )
+            quicksort ( list, i, max, per  )
